f6a4e01f1f215b3592b665e06c9e1a9052700644
AGABA ANDREW <agabaandre@gmail.com>
2022-09-21 20:31:19 +0300

    Added api methods

--- db_connector/mysqli_conn.php
+++ db_connector/mysqli_conn.php
@@ -1,8 +1,7 @@
 <?php
-$dbcon = mysqli_connect("localhost","memprow_meuser","zX#pAxzRt!","memprow_projects",'3306');
+$dbcon = mysqli_connect("localhost", "root", "", "memprow_project", '3306');
 
 // Check connection
-if (mysqli_connect_errno())
-  {
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+}
